258376813f2a70ffa0ef865d7ad3401e0b3ca21e
spikyshade <codeswithdevesh@gamil.com>
2023-09-19 11:24:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#About Us
+Welcome to our blog! We're a team of writers who are passionate about sharing our knowledge and experiences with the world. Our goal is to create informative and engaging content that helps our readers learn something new or gain a fresh perspective on a topic.
+
+#Our Mission
+We believe that knowledge is power, and we're committed to empowering our readers with the information they need to succeed. Whether you're a student, a professional, or just someone who's curious about the world, we're here to help you learn and grow.
+
+#Our Topics
+We cover a wide range of topics, from technology and science to health and wellness. Our writers are experts in their fields, and they bring a wealth of knowledge and experience to every article they write.
+
+#Our Team
+Sshade, Developer
+Sshade, Website tester
---
+++ blog/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from .models import Post
+from django.views.generic import (
+ListView,
+DetailView,
+CreateView,
+UpdateView,
+DeleteView
+)
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 10
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 10
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin,  UpdateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = '/'
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+def about(request):
+    return render(request, 'about.html', {'title':'About'})
